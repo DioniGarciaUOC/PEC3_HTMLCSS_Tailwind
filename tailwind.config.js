18852cb677368afa7cccfafd46fe050a387631e8
DioniGarciaUOC <dgarciagarcia6@uoc.edu>
2022-05-18 00:18:26 +0200

    almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      'dark-metal': '#444444',
+      'dark-gray': '#202020',
+      'white': 'white',
+      'muted': '#6c757d',
+      'rave-purple': 'rgb(140, 84, 177)'
+
+    },
+
+
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"]
+      },
+
+      backgroundImage: {
+        'metalic-skin': "linear-gradient(45deg, #444444, lightgrey, #585858)",
+        'industrial': "url('../images/detailed-background.jpg')",
+      }
+
+    },
   },
-  plugins: [],
+
 }
