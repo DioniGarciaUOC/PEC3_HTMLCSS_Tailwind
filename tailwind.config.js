65a88db16a2203d7f2dd853c20ce8b0fdd0ab69d
DioniGarciaUOC <dgarciagarcia6@uoc.edu>
2022-05-18 12:16:42 +0200

    Finished PEC

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,9 @@ module.exports = {
     },
 
     screens: {
-
-
       'sm': '576px',
-      // => @media (min-width: 576px) { ... }
-
       'md': '960px',
-      // => @media (min-width: 960px) { ... }
-
       'lg': '1440px',
-      // => @media (min-width: 1440px) { ... }
     },
 
 
